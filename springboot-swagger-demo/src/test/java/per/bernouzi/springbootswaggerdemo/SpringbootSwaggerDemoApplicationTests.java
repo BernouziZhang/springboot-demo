53d4a6ca470363411b701fc43976fdd525dbaf4a
{BernouziZhang} <{18130429131@163.com}>
2019-10-25 11:13:45 +0800

    Initial commit

---
+++ springboot-swagger-demo/src/test/java/per/bernouzi/springbootswaggerdemo/SpringbootSwaggerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package per.bernouzi.springbootswaggerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSwaggerDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
